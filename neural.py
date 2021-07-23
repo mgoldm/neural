def6829eecd1e70735490f314916d0abff616274
mgoldm <57058926+mgoldm@users.noreply.github.com>
2021-07-23 10:30:45 +0300

    Update neural.py

--- neural.py
+++ neural.py
@@ -1,7 +1,9 @@
-#подключаем библиотеку numpy
+# подключаем библиотеку numpy
 import numpy
-#библиотека с сигмоидой 
+# библиотека с сигмоидой
 import scipy.special
+
+
 # Определение класса нейронной сети
 class neuralNetwork:
     # инициализация нейронной сети
@@ -12,27 +14,54 @@ class neuralNetwork:
         self.onodes = outputnodes
         # коэффициент обучения
         self.lr = learningrate
-        #Матрица весовых коэффициентов связей wih и who
-        #Весовые коэффициенты связей между узлом i и узлом j следующего слоя обозначены, как w_i_j
-        self.wih=numpy.random.normal(0.0, pow(self.hnodes, -0,5), (self.hnodes, self. inodes))
+        # Матрица весовых коэффициентов связей wih и who
+        # Весовые коэффициенты связей между узлом i и узлом j следующего слоя обозначены, как w_i_j
+        self.wih = numpy.random.normal(0.0, pow(self.hnodes, -0, 5), (self.hnodes, self.inodes))
         self.who = numpy.random.normal(0.0, pow(self.onodes, -0, 5), (self.onodes, self.hnodes))
-        #Использоваение сигмоиды в качестве функции активации
+        # Использоваение сигмоиды в качестве функции активации
         self.activation_function = lambda x: scipy.special.exipt(x)
         pass
 
-    def train(self):
+    def train(self, inputs_list, targets_list):
+        # преобразовать список входных значений в двухмерный массив
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        targets = numpy.array(targets_list, ndmin=2).T
+
+        # рассчитать входящие сигналы для скрытого слоя
+        hidden_inputs = numpy.dot(self.wih, inputs)
+        # рассчитать исходящие сигналы для скрытого слоя
+        hidden_outputs = self.activation_function(hidden_inputs)
+
+        # рассчитать входящие сигналы для выходного слоя
+        final_inputs = numpy.dot(self.who, hidden_outputs)
+        # рассчитать исходящие сигналы для выходного слоя
+        final_outputs = self.activation_function(final_inputs)
+
+        # ошибка выходного слоя = (целевое значение-фактическое значение)
+        output_errors = targets - final_outputs
+        # ошибка скрытого слоя - это ошибки output_errors,
+        #распределение пропорционально весовым коэффициентам связей и рекомбинированные на скрытых узлах
+        hidden_errors = numpy.dot(self.who.T, output_errors)
+
+        # обновим весовые коэффициенты для связей между скрытым и выходным слоями
+        self.who += self.lr * numpy.dot((output_errors * final_outputs * (1.0 - final_outputs)),
+                                        numpy.transpose(hidden_outputs))
+
+        #обновить весовые коэффициенты для связей между крытым и выходным слоями
+        self.wih += self.lr * numpy.dot((hidden_errors * hidden_outputs * (1.0 - hidden_outputs)),
+                                        numpy.transpose(inputs))
         pass
 
     def query(self, inputs_list):
-        #преобразовать список входных значений в двумерный массив
-        inputs = numpy.array(inputs_list, ndmin=2)
-        #расчитать входящие сигналы для скрытого поля
+        # преобразовать список входных значений в двумерный массив
+        inputs = numpy.array(inputs_list, ndmin=2).T
+        # расчитать входящие сигналы для скрытого поля
         hidden_inputs = numpy.dot(self.wih, inputs)
-        #расчитать исходящие сигналы для скрытого поля
+        # расчитать исходящие сигналы для скрытого поля
         hidden_outputs = self.activation_function(hidden_inputs)
-        #расчитать входящие сигналы для выходного поля
+        # расчитать входящие сигналы для выходного поля
         final_inputs = numpy.dot(self.who, hidden_outputs)
-        #расчитать исходящие сигналы для выходного поля
+        # расчитать исходящие сигналы для выходного поля
         final_outputs = self.activation_function(final_inputs)
         return final_outputs
         pass
@@ -44,5 +73,5 @@ hidden_nodes = 3
 output_nodes = 3
 # коэффициент обучения
 learning_grate = 3
-#Создаем экзэмпляр нейронной сети
-n = neuralNetwork(input_nodes, hidden_nodes,output_nodes, learning_grate)
+# Создаем экзэмпляр нейронной сети
+n = neuralNetwork(input_nodes, hidden_nodes, output_nodes, learning_grate)
